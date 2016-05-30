782f371592cf9a79383dabf4f25ed721406a00f0
Matteo Bruni <ar3s@icloud.com>
2016-05-30 16:45:42 +0200

    startup

---
+++ UnitOfWork.NET.VelocityDB.NUnit.Data/MyClass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace UnitOfWork.NET.VelocityDB.NUnit.Data
+{
+	public class MyClass
+	{
+		public MyClass()
+		{
+		}
+	}
+}
+
---
+++ UnitOfWork.NET.VelocityDB/MyClass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace UnitOfWork.NET.VelocityDB
+{
+	public class MyClass
+	{
+		public MyClass()
+		{
+		}
+	}
+}
+
